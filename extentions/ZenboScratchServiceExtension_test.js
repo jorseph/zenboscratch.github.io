db416a4d7dc81a37472188a439cd5ade7d30dbf9
zzenboscratch <zenboscratch@gmail.com>
2017-08-16 17:29:07 +0800

    Site updated: 2017-08-16 17:29:07

--- extentions/ZenboScratchServiceExtension_test.js
+++ extentions/ZenboScratchServiceExtension_test.js
@@ -17,48 +17,10 @@
 
     ext.Setting_targetIP = function (ip) {
         console.log("Setting_targetIP");
-
-/*		
-            $.ajax({
-            url: 'http://' + ip + port + '/?name=Add_and_update_sentence' + '&p1=' + 'test' + '&p2=' + 'zenbo',
-            dataType: 'text',
-            crossDomain: true,
-            success: function (data) {
-            console.log("success handler");
-				
-			var setupFlag = true;
-			var flagIndex = 0;   
-
-			for(var i=0; i < flagArray.data.length; i++){
-
-				 if ( ip == flagArray.data[i].device) {    
-					  setupFlag = false;
-					  flagIndex = i;  
-					  console.log("false " + "flagIndex: "+ flagIndex);
-				 }     
-			}
-		  
-			if (setupFlag == true) {
-				flagArray.data.push( { device: ip, correctedSentence: "", sentence_1_flag: false, sentence_2_flag: false, sentence_3_flag: false, sentence_4_flag: false, sentence_5_flag: false, number_flag: false, get_sentences_flag: true } );
-				console.log("add new device IP and its flags");
-				flagIndex = flagArray.data.length -1 ;  
-				console.log("true " + "flagIndex: "+ flagIndex);
-			 }
-			 
-			 getSentencesRecursion(ip, flagIndex); 
-				
-
-            },
-            error: function (jqXHR, textStatus, errorThrown) {
-                console.log("error handler");
-            }
-        });
-		
-*/		
         return ip;
     };
 
-	var getSentencesRecursion = function(ip, flagIndex) {
+    var getSentencesRecursion = function(ip, flagIndex) {
 
 		if ( flagArray.data[flagIndex].get_sentences_flag === true ) {  	 
 			 flagArray.data[flagIndex].get_sentences_flag = false;  
