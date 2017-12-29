96a2e4e48cbe5ca9d33690ac2637a1f4041588cc
ZenboScratch <zenboscratch@gmail.com>
2017-12-29 12:14:41 +0800

    Update ZenboScratchServiceExtensionAdvance_test.js

--- extentions/ZenboScratchServiceExtensionAdvance_test.js
+++ extentions/ZenboScratchServiceExtensionAdvance_test.js
@@ -1067,7 +1067,237 @@ ext.Add_and_update_sentence_number = function (ip) {
         });
     };
 
+    var TRANSLATIONS = {
+        us: {
+            Setting_targetIP: 'Set Zenbo IP: %s',
+            Body_movement: 'IP %s move %m.move_direction by %m.move_far Meter %m.move_speed',
+            Stop_moving: 'IP %s stop action',
+            Head_movement: 'IP %s turn head to the %m.head_direction by %m.head_degree degree(s)',
+            Body_turn: 'IP %s turn body to the  %m.body_turn_direction by %m.body_turn_degree degree(s)',
+            Remote_control_body: 'IP %s control body to %m.remote_control_body',
+            Action: 'IP %s Zenbo do the action %m.action_type',
+            Cancel_actionset: 'IP %s stop the canned action',
+            Facial: 'IP %s make the %m.facial_type expression', 
+            hideFace: 'IP %s hide the expression',
+            TTS: 'IP %s speak:  %m.tts_type ',
+	    TTS_editor: 'IP %s speak:  %s',
+	    Adjust_stream_volume: 'IP %s make %m.volume_option_type volume %m.volume_type',
+	    Adjust_tts_and_speed: 'IP %s speak: %s by %m.tts_speed_type speed',
+	    Add_and_update_sentence: 'IP %s is ready to listen %m.sentence_type which is %s',
+	    Speak_and_listen: 'IP %s would start listening',
+	    when_listen_and_run: 'When IP %s heard %m.sentence_type',
+	    Delete_instance: 'IP %s delete all sentences',
+	    Add_and_update_sentence_number: 'IP %s is ready to listen to numbers',
+	    when_listen_number_and_run: 'When IP %s heard numbers',
+	    getCorrectedSentence: 'Current number heard by IP %s',
+	    playVideosInYoutube: 'IP %s %m.playVideosInYoutubeItems playing a Youtube URL: %s',
+	    playUrlMusic: 'IP %s %m.playUrlMusicItems playing an online audio file: %s',
+	    displayUrlPictures: 'IP %s %m.displayUrlPicturesItems browsing an online image file: %s',
+	    openDriveVideoUrl: 'IP %s %m.openDriveVideoUrlItems playing a Google Drive video shareable link: %s',
+	    openDriveAudioUrl: 'IP %s %m.openDriveAudioUrlItems playing Google Drive audio shareable link: %s',
+	    openDrivePictureUrl: 'IP %s %m.openDrivePictureUrlItems browsing a Google Drive image shareable link: %s',
+        openDriveDocumentUrl: 'IP %s %m.openDriveDocumentUrlItems browsing a Google Drive document shareable link: %s',
+		when_touch_head_and_run: 'When the head of IP %s is touched',
+		recordAudio: 'IP %s %m.recordAudioItems audio recording, file name: %s',
+		recordVideo: 'IP %s %m.recordVideoItems %m.recordVideoSizes video recording, file name: %s',
+	    head_direction: ["left", "right", "top", "bottom"],
+            head_degree: ["0", "15", "30", "45"],
+            move_direction: ["forward", "backward"],
+            move_far: ["0.25", "0.50", "0.75", "1.00", "1.25", "1.50", "1.75", "2.00"],
+            move_speed: ["slowly", "normally", "fast"],
+            body_turn_direction: ["left", "right"],
+            body_turn_degree: ["0", "15", "30", "45", "60", "75", "90", "105", "120", "135", "150", "165", "180", "195", "210", "225", "240", "255", "270", "285", "300", "315", "330", "345", "360"],
+            remote_control_body: ["stop", "move forward", "turn left", "turn right"],
+            action_type: ["relax(default)", "listen to the command/briefing(high)", "complete the task/satisfaction", "greet", "there is a general message-1", "standby(boring)",
+                          "charge(satisfaction)", "listen to the command/briefing(low)", "there are continuous task conflicts", "be disappointed(user finishing)", 
+                          "be frustrated(user finishing)", "standby(tired)", "there is no electricity soon", "charge(pleasure)", "charge(lazy)", "dance-1 more powerfully", 
+                          "understand", "play music", "turn left slowly", "turn left fast", "shake the head(a negative answer)", "dance-1 more powerlessly", "be shy", 
+                          "do not understand", "dance-2", "shake the head continuously", "shake and twist the head", "dance-fast", "shake the head", "finish greeting", 
+                          "there is a general message-2", "turn right slowly", "turn right fast", "end turning left slowly and reverse", "end turning right slowly and reverse",
+                          "end turning left fast and reverse", "end turning right fast and reverse",  "eliminate continuous task conflicts"], 
+            facial_type: ["interested", "doubting", "proud", "defualt", "happy", "expecting", "shocked", "questioning", "impatient", "confident", "active", "pleased", 
+                          "helpless", "serious", "worried", "pretending", "lazy", "aware_R", "tired", "shy", "innocent", "singing", "aware_L", "default_still"],
+            tts_type: ["Hi, welcome to zenbo scratch service", "Look here", "WoW", "YA"],
+            volume_type: ["louder", "quieter"],
+            music_type: ["start", "pause", "resume", "stop", "restart"],
+            volume_option_type: ["music", "alarm clock", "notification", "robot speaking"],
+            tts_speed_type: ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8"],
+            sentence_type: ["sentence1", "sentence2", "sentence3", "sentence4", "sentence5"],
+            playVideosInYoutubeItems: ["start", "end"],
+            displayUrlPicturesItems: ["start", "end"],
+            playUrlMusicItems: ["start", "end"],
+            openDriveVideoUrlItems: ["start", "end"],
+            openDriveAudioUrlItems: ["start", "end"],
+            openDrivePictureUrlItems: ["start", "end"],
+            openDriveDocumentUrlItems: ["start", "end"],
+	        ttsEditorDefaultText: 'please fill the sentence',
+	        defaultAddedSentence: 'eating',
+ 			recordAudioItems: ["start", "end"],
+			recordVideoItems: ["start", "end"],
+			recordVideoSizes: ["720P", "480P", "240P"],
+        },
+	tw: {
+            Setting_targetIP: '設定Zenbo IP: %s',
+            Body_movement: 'IP %s 移動 %m.move_direction %m.move_far 公尺 %m.move_speed 速度',
+            Stop_moving: 'IP %s 停止動作',
+            Head_movement: 'IP %s 轉動頭部 向 %m.head_direction %m.head_degree 度',
+            Body_turn: 'IP %s 轉動身體 向 %m.body_turn_direction %m.body_turn_degree 度',
+            Remote_control_body: 'IP %s 控制身體 %m.remote_control_body',
+            Action: 'IP %s Zenbo 做動作 %m.action_type',
+            Cancel_actionset: 'IP %s 停止罐頭動作',
+            Facial: 'IP %s 做出表情 %m.facial_type', 
+            hideFace: 'IP %s 隱藏表情',
+            TTS: 'IP %s 說話 %m.tts_type',
+	    TTS_editor: 'IP %s 說話 %s',
+	    Adjust_stream_volume: 'IP %s 調整 %m.volume_option_type 音量 %m.volume_type',
+	    Adjust_tts_and_speed: 'IP %s 說話 %s 速度 %m.tts_speed_type',
+	    Add_and_update_sentence: 'IP %s 準備要聽 %m.sentence_type 是 %s',
+	    Speak_and_listen: 'IP %s 我要開始聽',
+	    when_listen_and_run: '當我聽到 IP %s 的 %m.sentence_type',
+	    Delete_instance: 'IP %s 刪除全部語句',
+	    Add_and_update_sentence_number: 'IP %s 準備要聽數字',
+	    when_listen_number_and_run: '當我聽到 IP %s 的數字',
+	    getCorrectedSentence: '目前 IP %s 聽到的數字 ',
+	    playVideosInYoutube: 'IP %s %m.playVideosInYoutubeItems 播放 Youtube 網址: %s',
+	    playUrlMusic: 'IP %s %m.playUrlMusicItems 播放線上音樂: %s',
+	    displayUrlPictures: 'IP %s %m.displayUrlPicturesItems 瀏覽線上圖片: %s',
+	    openDriveVideoUrl: 'IP %s %m.openDriveVideoUrlItems 播放 Google Drive 影片: %s',
+	    openDriveAudioUrl: 'IP %s %m.openDriveAudioUrlItems 播放 Google Drive 音樂: %s',
+	    openDrivePictureUrl: 'IP %s %m.openDrivePictureUrlItems 瀏覽 Google Drive 圖片: %s',
+            openDriveDocumentUrl: 'IP %s %m.openDriveDocumentUrlItems 瀏覽 Google Drive 文件: %s',
+		when_touch_head_and_run: '當摸到 IP %s 的頭',
+		recordAudio: 'IP %s %m.recordAudioItems 錄音, 檔名: %s',
+		recordVideo: 'IP %s %m.recordVideoItems %m.recordVideoSizes 錄影, 檔名: %s',
+	    head_direction: ["左", "右", "上", "下"],
+            head_degree: ["0", "15", "30", "45"],
+            move_direction: ["前進", "後退"],
+            move_far: ["0.25", "0.50", "0.75", "1.00", "1.25", "1.50", "1.75", "2.00"],
+            move_speed: ["慢", "一般", "快"],
+            body_turn_direction: ["左轉", "右轉"],
+            body_turn_degree: ["0", "15", "30", "45", "60", "75", "90", "105", "120", "135", "150", "165", "180", "195", "210", "225", "240", "255", "270", "285", "300", "315", "330", "345", "360"],
+            remote_control_body: ["停止", "前進", "左轉", "右轉"],
+            action_type: ["輕鬆(預設)", "Listen to the command/briefing(high)", "Complete the task/satisfaction", "打招呼", "一般訊息一", "待機(無聊)", "充電(滿足)", "聽指令/briefing(矮)", "連續任務衝突", "失望(使用者結束)",
+                            "沮喪(使用者結束)", "待機(疲倦)", "快沒電", "充電(愉悅)", "充電(慵懶)", "跳舞一大點", "聽懂", "音樂播放", "向左慢轉", "向左急轉", "搖頭(回答否定)", "跳舞一小點", 
+                            "害羞", "聽不懂", "跳舞二", "連續搖頭", "搖頭晃腦", "跳舞極快", "搖頭", "結束打招呼", "一般訊息二", "向右慢轉", "向右急轉", "結束向左慢轉", "結束向右慢轉",
+                            "結束向左急轉", "結束向右急轉", "解除連續任務衝突"],
+            facial_type: ["有興趣", "疑惑", "驕傲", "輕鬆愉快(預設)", "開心", "期待", "愣一下", "質疑", "不耐煩", "自信", "有活力一", "得意", "無奈", "嚴肅", "煩惱", "裝平靜", "慵懶", "察覺", 
+                            "倦怠", "害羞", "無辜", "有活力二", "察覺", "預設"],
+            tts_type: ["Hi,你好", "看這裡", "WoW", "YA"],
+            volume_type: ["大聲點", "小聲點"],
+            music_type: ["開始", "暫停", "繼續", "停止", "重新"],
+            volume_option_type: ["音樂", "鬧鐘", "通知", "說話"],
+            tts_speed_type: ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8"],
+            sentence_type: ["語句一", "語句二", "語句三", "語句四", "語句五"],
+            playVideosInYoutubeItems: ["開始", "關閉"],
+            displayUrlPicturesItems: ["開始", "關閉"],
+            playUrlMusicItems: ["開始", "關閉"],
+            openDriveVideoUrlItems: ["開始", "關閉"],
+            openDriveAudioUrlItems: ["開始", "關閉"],
+            openDrivePictureUrlItems: ["開始", "關閉"],
+            openDriveDocumentUrlItems: ["開始", "關閉"],
+   	        ttsEditorDefaultText: '請填入文字',
+            defaultAddedSentence: '吃飯',
+ 			recordAudioItems: ["開始", "關閉"],
+			recordVideoItems: ["開始", "關閉"],
+			recordVideoSizes: ["720P", "480P", "240P"],
+        },  		
+    }	
+	
 
+    function getTranslationForLang( lang ){
+        switch (lang){
+               case "en":
+		    return TRANSLATIONS.us;
+	       case "en-us":
+		    return TRANSLATIONS.us; 			
+               case "zh":
+		    return TRANSLATIONS.tw; 	
+               case "zh-tw":
+		    return TRANSLATIONS.tw; 
+               default:
+                    return TRANSLATIONS.us;
+            
+        }
+    }
+	
+    // how which language translation is chosen (increasing priority):
+    //   1 - explicit 'lang' parameter in the url (e.g: http://scratchx.org/?url=https://zenboscratchservice.github.io/extentions/ZenboScratchServiceExtensionAdvance.js&lang=en#scratch)
+    //   2 - browser first preferred language (navigator.languages[0])
+    //   3 - default (en-us)
+		
+    var urlParams = new URLSearchParams(window.location.search);
+    var lang = ( urlParams.get('lang') || navigator.browserLanguage || navigator.language ).toLowerCase();
+    console.log("lang:" + lang);
+	
+    var translate = getTranslationForLang(lang);
+
+	    var descriptor = {
+        blocks: [
+            ['w', translate.Setting_targetIP, 'Setting_targetIP', "192.168.0.1"],
+            ['', translate.Body_movement, 'Body_movement', "192.168.0.1", translate.move_direction[0], translate.move_far[0], translate.move_speed[0]],
+            ['', translate.Stop_moving, 'Stop_moving', "192.168.0.1"],
+            ['', translate.Head_movement, 'Head_movement', "192.168.0.1", translate.head_direction[0], translate.head_degree[3]], 
+            ['', translate.Body_turn, 'Body_turn', "192.168.0.1", translate.body_turn_direction[0], translate.body_turn_degree[6]],
+            ['', translate.Remote_control_body, 'Remote_control_body', "192.168.0.1", translate.remote_control_body[3]],
+          //  ['', translate.Action, 'Action', "192.168.0.1", translate.action_type[3]],
+          //  ['', translate.Cancel_actionset, 'Cancel_actionset', "192.168.0.1"],
+            ['', translate.Facial, 'Facial', "192.168.0.1", translate.facial_type[5]],
+            ['', translate.hideFace, 'hideFace', "192.168.0.1"],
+            ['', translate.TTS, 'TTS', "192.168.0.1", translate.tts_type[0]],
+            ['', translate.TTS_editor, 'TTS_editor', "192.168.0.1", translate.ttsEditorDefaultText],
+            ['', translate.Adjust_stream_volume, 'Adjust_stream_volume', "192.168.0.1", translate.volume_option_type[3], translate.volume_type[0]],
+            ['', translate.Adjust_tts_and_speed, 'Adjust_tts_and_speed', "192.168.0.1", translate.ttsEditorDefaultText, translate.tts_speed_type[1]],
+            ['', translate.Add_and_update_sentence, 'Add_and_update_sentence', "192.168.0.1", translate.sentence_type[0], translate.defaultAddedSentence], 
+            ['', translate.Speak_and_listen, 'Speak_and_listen', "192.168.0.1"], 
+            ['h', translate.when_listen_and_run, 'when_listen_and_run', "192.168.0.1", translate.sentence_type[0]],
+            ['', translate.Delete_instance, 'Delete_instance', "192.168.0.1"],
+          //  ['', translate.Add_and_update_sentence_number, 'Add_and_update_sentence_number', "192.168.0.1"],
+            ['h', translate.when_listen_number_and_run, 'when_listen_number_and_run', "192.168.0.1"],
+            ['r', translate.getCorrectedSentence, 'getCorrectedSentence', "192.168.0.1"],
+            ['', translate.playVideosInYoutube, 'playVideosInYoutube', "192.168.0.1", translate.playVideosInYoutubeItems[0], 'https://www.youtube.com/watch?v=Ou21RusvBcg'],
+            ['', translate.playUrlMusic, 'playUrlMusic', "192.168.0.1", translate.playUrlMusicItems[0], 'https://zenboscratch.github.io/examples/zenbo_music.mp3'],
+            ['', translate.displayUrlPictures, 'displayUrlPictures', "192.168.0.1", translate.displayUrlPicturesItems[0], 'https://zenboscratch.github.io/images/zenbo.jpg'],
+            ['', translate.openDriveVideoUrl, 'openDriveVideoUrl', "192.168.0.1", translate.openDriveVideoUrlItems[0], 'https://drive.google.com/open?id=0B5o6VwYT7NaiUUhrTW12Ql9tcUk'],
+            ['', translate.openDriveAudioUrl, 'openDriveAudioUrl', "192.168.0.1", translate.openDriveAudioUrlItems[0], 'https://drive.google.com/open?id=0B5o6VwYT7NaibHJ0LWtHN0JtVFU'], 
+            ['', translate.openDrivePictureUrl, 'openDrivePictureUrl', "192.168.0.1", translate.openDrivePictureUrlItems[0], 'https://drive.google.com/open?id=0B5o6VwYT7NaiSVJ0S3JKeEZwODA'],
+            ['', translate.openDriveDocumentUrl, 'openDriveDocumentUrl', "192.168.0.1", translate.openDriveDocumentUrlItems[0], 'https://drive.google.com/open?id=0B5o6VwYT7NaiN1h3SXZHTjRsc2s'],
+			['h', translate.when_touch_head_and_run, 'when_touch_head_and_run', "192.168.0.1"],
+			['', translate.recordAudio, 'recordAudio', "192.168.0.1", translate.recordAudioItems[0], 'testAudio'],
+            ['', translate.recordVideo, 'recordVideo', "192.168.0.1", translate.recordVideoItems[0], translate.recordVideoSizes[0], 'testVideo'],  
+        ],
+        menus: {
+            "head_direction": translate.head_direction,
+            "head_degree": translate.head_degree,
+            "move_direction": translate.move_direction,
+            "move_far": translate.move_far,
+            "move_speed": translate.move_speed,
+            "body_turn_direction": translate.body_turn_direction,
+            "body_turn_degree": translate.body_turn_degree,
+            "remote_control_body": translate.remote_control_body,
+            "action_type": translate.action_type,
+            "facial_type": translate.facial_type,
+            "tts_type": translate.tts_type,
+            "volume_type": translate.volume_type,
+            "music_type": translate.music_type,
+            "volume_option_type": translate.volume_option_type,
+            "tts_speed_type": translate.tts_speed_type,
+            "sentence_type": translate.sentence_type,
+            "playVideosInYoutubeItems": translate.playVideosInYoutubeItems,
+            "displayUrlPicturesItems": translate.displayUrlPicturesItems,
+            "playUrlMusicItems": translate.playUrlMusicItems,
+            "openDriveVideoUrlItems": translate.openDriveVideoUrlItems,
+            "openDriveAudioUrlItems": translate.openDriveAudioUrlItems,
+            "openDrivePictureUrlItems": translate.openDrivePictureUrlItems,
+            "openDriveDocumentUrlItems": translate.openDriveDocumentUrlItems,
+			"recordAudioItems": translate.recordAudioItems,
+			"recordVideoItems": translate.recordVideoItems,
+			"recordVideoSizes": translate.recordVideoSizes,		
+        },
+        url: 'https://zenboscratchservice.github.io/' // Link to extension documentation, homepage, etc.
+    };
+	
+	
+	/*
     var descriptor = {
         blocks: [
             ['w', '設定Zenbo IP: %s', 'Setting_targetIP', "192.168.0.1"],
@@ -1135,7 +1365,7 @@ ext.Add_and_update_sentence_number = function (ip) {
         },
         url: 'https://zenboscratchservice.github.io/' // Link to extension documentation, homepage, etc.
     };
-
+    */     
     // Register the extension
     ScratchExtensions.register('ZenboScratchService', descriptor, ext);
 })({});
