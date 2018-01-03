298f5c34be3aa0252f09e4b676c87e99f50750f1
ZenboScratch <zenboscratch@gmail.com>
2018-01-03 12:54:24 +0800

    Update ZenboScratchServiceExtensionEducation_test2.js

--- extentions/ZenboScratchServiceExtensionEducation_test2.js
+++ extentions/ZenboScratchServiceExtensionEducation_test2.js
@@ -274,8 +274,8 @@ function showMessage(message)
 	    "background-color": "#e7e7e7",
 	    "color": "black",
 	    "border": "2px solid black", 	
-	    "width": "40px", 	
-            "height": "40px",
+	    "width": "80px", 	
+            "height": "20px",
 	    "font-size": "16px",
 	    "text-align": "center" 	
         }); 
